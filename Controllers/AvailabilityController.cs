4baaef3328245808356f90b834123de86bc6f174
jkodumagullaP <58768596+jkodumagullaP@users.noreply.github.com>
2025-12-15 21:38:36 +0530

    Update AvailabilityController.cs

--- Controllers/AvailabilityController.cs
+++ Controllers/AvailabilityController.cs
@@ -1,31 +1,40 @@
-[Authorize(Roles = "LeadAssessor")]
-public class AvailabilityController : Controller
+using CAT.AID.Web.Data;
+using CAT.AID.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CAT.AID.Web.Controllers
 {
-    private readonly ApplicationDbContext _db;
-
-    public AvailabilityController(ApplicationDbContext db)
+    [Authorize]
+    public class AvailabilityController : Controller
     {
-        _db = db;
-    }
+        private readonly ApplicationDbContext _db;
 
-    [HttpGet]
-    public async Task<IActionResult> GetAvailableAssessors(
-        DateOnly date, TimeSpan from, TimeSpan to)
-    {
-        var available = await _db.AssessorAvailabilities
-            .Include(a => a.Assessor)
-            .Where(a =>
-                a.Date == date &&
-                a.SlotFrom <= from &&
-                a.SlotTo >= to &&
-                !a.IsBooked)
-            .Select(a => new
-            {
-                a.AssessorId,
-                a.Assessor.FullName
-            })
-            .ToListAsync();
+        public AvailabilityController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
 
-        return Json(available);
+        [HttpGet]
+        public async Task<IActionResult> GetAvailableAssessors(DateOnly date, TimeSpan from, TimeSpan to)
+        {
+            var assessors = await _db.AssessorAvailabilities
+                .Include(a => a.Assessor)
+                .Where(a =>
+                    a.Date == date &&
+                    a.SlotFrom <= from &&
+                    a.SlotTo >= to &&
+                    !a.IsBooked)
+                .Select(a => new
+                {
+                    a.AssessorId,
+                    a.Assessor.FullName
+                })
+                .Distinct()
+                .ToListAsync();
+
+            return Json(assessors);
+        }
     }
 }
